a19cc8f549a46cbd3f4b057ea1ae1a1257522a83
NAVEEN GEORGE JOSEPH <naveengjoseph@gmail.com>
2023-03-05 02:41:14 +0530

    added fifth page

--- static/index.js
+++ static/index.js
@@ -13,15 +13,18 @@ one = document.getElementById("one");
 two = document.getElementById("two");
 three = document.getElementById("three");
 four = document.getElementById("four");
+five = document.getElementById("five");
 wh1 = wh / 2;
 wh2 = wh + wh / 2;
 wh3 = 2 * wh + wh / 2;
+wh4 = 3 * wh + wh / 2;
 window.onscroll = function () {
   if (window.scrollY < wh1) {
     one.classList.add("active");
     two.classList.remove("active");
     three.classList.remove("active");
     four.classList.remove("active");
+    five.classList.remove("active");
     mybutton.style.display = "none";
     creator.style.bottom = "15px";
   } else if (window.scrollY > wh1 && window.scrollY < wh2) {
@@ -29,6 +32,7 @@ window.onscroll = function () {
     one.classList.remove("active");
     three.classList.remove("active");
     four.classList.remove("active");
+    five.classList.remove("active");
     mybutton.style.display = "block";
     creator.style.bottom = "100px";
   } else if (window.scrollY > wh2 && window.scrollY < wh3) {
@@ -36,13 +40,23 @@ window.onscroll = function () {
     one.classList.remove("active");
     two.classList.remove("active");
     four.classList.remove("active");
+    five.classList.remove("active");
     mybutton.style.display = "block";
     creator.style.bottom = "100px";
   } else if (window.scrollY > wh3) {
     four.classList.add("active");
     one.classList.remove("active");
-    three.classList.remove("active");
     two.classList.remove("active");
+    three.classList.remove("active");
+    five.classList.remove("active");
+    mybutton.style.display = "block";
+    creator.style.bottom = "100px";
+  } else if (window.scrollY > wh4) {
+    five.classList.add("active");
+    one.classList.remove("active");
+    two.classList.remove("active");
+    three.classList.remove("active");
+    four.classList.remove("active");
     mybutton.style.display = "block";
     creator.style.bottom = "100px";
   }
